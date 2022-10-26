1dec62100a4f53fa5d7849b43478f013c0e8ebfc
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-10-26 16:19:42 -0700

    made walking class and TIMES migrate

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+TIMES = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('N', 'Night')
+)
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -13,3 +20,17 @@ class Dog(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+
+class Walking(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=TIMES,
+        default=TIMES[0][0]
+    )
+
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
+    
